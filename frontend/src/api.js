943d683ad3d517c2c3ad2c1ffd81911294fcaf8f
bhagirathpaliyal <bhagirathpaliyal@gmail.com>
2025-02-01 12:21:24 +0530

    Deploy app to GitHub Pages

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/notes";
+const API_URL = "https://mernnotesapp-1.onrender.com/api/notes";
 
 export const getNotes = async () => axios.get(API_URL);
 export const addNote = async (note) => axios.post(API_URL, note);
 export const updateNote = async (id, note) => axios.put(`${API_URL}/${id}`, note);
 export const deleteNote = async (id) => axios.delete(`${API_URL}/${id}`);
+
+
